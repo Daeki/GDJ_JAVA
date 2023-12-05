5f9e1bf8805b4114d361736e3cbfc6b636150809
Daeki <kimdaeki@gmail.com>
2023-12-05 14:16:12 +0900

    add

--- src/com/winter/study1/Study3For.java
+++ src/com/winter/study1/Study3For.java
@@ -1,15 +1,42 @@
 package com.winter.study1;
 
+import java.util.Scanner;
+
 public class Study3For {
 	
 	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		//
+		//0-59분
+		//0-59초
+		////1분 5초
+		//분과 초를 입력
 		
-		//구구단
-		for(int i=2;i<10;i++) {
-			for(int j=1;j<10;j++) {
-				System.out.println(i+" * "+j +" = "+i*j);
+		int min=1;
+		int sec=5;
+		
+		//반복문을 종료 하는 방법
+		//1. break를 만났을 때
+		//2. 조건식이 false일때
+		boolean flag=false;
+		for(int m=0;m<60;m++) {
+			for(int s=0;s<60;s++) {
+				System.out.println(m+"분 : "+s+"초");
+				if(min==m && sec==s) {
+					flag=!flag;
+					m=59;
+					break;
+				}
 			}
+			
+//			if(flag) {
+//				break;
+//			}
+			
 		}
+		
+
+		
 		
 		
 		
---
+++ src/com/winter/study1/Study4For.java
@@ -0,0 +1,10 @@
+package com.winter.study1;
+
+public class Study4For {
+	
+	public static void main(String [] args) {
+		
+		
+	}
+
+}
