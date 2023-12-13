bd23527342d51fabc091bfe6192e1ebc899f32a0
Daeki <kimdaeki@gmail.com>
2023-12-13 17:09:31 +0900

    1

--- src/com/winter/study3/ex/CardController.java
+++ src/com/winter/study3/ex/CardController.java
@@ -7,8 +7,11 @@ public class CardController {
 	
 	public void start() {
 		Scanner sc = new Scanner(System.in);
-		boolean check=true;
+		CardView cardView = new CardView();
+		CardManager cardManager = new CardManager();
 		
+		boolean check=true;
+		Card [] cards = null;
 		while(check) {
 			System.out.println("1. 목록보기");
 			System.out.println("2. 상세보기");
@@ -20,10 +23,13 @@ public class CardController {
 			
 			if(select==1) {
 				System.out.println("목록");
+				cardView.view(cards);
 			}else if(select==2) {
 				System.out.println("디테일");
+				cardView.detail(cards);
 			}else if(select==3) {
 				System.out.println("추가");
+				cards = cardManager.addCard(cards);
 			}else if(select==4) {
 				System.out.println("수정");
 			}else if(select==5) {
--- src/com/winter/study3/ex/ExMain.java
+++ src/com/winter/study3/ex/ExMain.java
@@ -4,16 +4,16 @@ public class ExMain {
 
 	public static void main(String[] args) {
 		
-		//CardController cardController = new CardController();
-		//cardController.start();
-		CardManager cardManager = new CardManager();
-		Card [] cards = new Card[1];
-		Card card = new Card();
-		card.setNum(1);
-		card.setName("winter");
-		cards[0]=card;
-		
-		cardManager.addCard(cards);
+		CardController cardController = new CardController();
+		cardController.start();
+//		CardManager cardManager = new CardManager();
+//		Card [] cards = new Card[1];
+//		Card card = new Card();
+//		card.setNum(1);
+//		card.setName("winter");
+//		cards[0]=card;
+//		
+//		cardManager.addCard(cards);
 		
 		
 
