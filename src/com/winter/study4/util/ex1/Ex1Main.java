2f13697a9a31929ed3063e4c1d3a0c529fa8cd65
Daeki <kimdaeki@gmail.com>
2023-12-15 17:15:54 +0900

    1

--- src/com/winter/study4/util/ex1/Ex1Main.java
+++ src/com/winter/study4/util/ex1/Ex1Main.java
@@ -3,7 +3,11 @@ package com.winter.study4.util.ex1;
 public class Ex1Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		
+		
+		new StudentController().start();
 
 	}
 
